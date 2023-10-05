2cd4068129930474d2e0dc855ba2dad5a5c50978
linwei0907 <126299856+linwei0907@users.noreply.github.com>
2023-10-05 19:11:17 -0400

    Create 9. Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+
+        for i in range(len(x) // 2):
+            if x[i] != x[len(x) - 1 - i]:
+                return False
+
+        return True
