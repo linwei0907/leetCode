f64029c753e41a3b101bf87569025807dd9c8b8e
linwei0907 <126299856+linwei0907@users.noreply.github.com>
2023-10-06 02:48:56 -0400

    Create 66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+            if digits[i]+1 != 10:
+                digits[i] += 1
+                break
+            else:
+                digits[i] = 0
+                if i == 0:
+                    digits.insert(0, 1)
+        return digits
